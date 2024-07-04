b71f48acc3c3a9f33e14a772dd0040c0ab025135
Mohamed Hatem <83113802+MohamedHatem-0110@users.noreply.github.com>
2024-07-04 07:45:42 +0300

    commit

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ wss.on('connection', (ws) => {
 
 app.post('/sendMessage', (req, res) => {
     const message = req.body.message;
+    console.log(message)
     wss.clients.forEach(client => {
         if (client.readyState === WebSocket.OPEN) {
             client.send(message);
